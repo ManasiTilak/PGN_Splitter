e5345ded72a05a1215b702344fcace4dccb45475
unknown <manasitilak12@gmail.com>
2023-12-05 20:13:12 +0530

    initial commit

---
+++ pgn_splitter.py
@@ -0,0 +1,46 @@
+import chess.pgn
+import os
+
+def split_pgn_into_chunks(input_file_path, output_folder, chunk_size_mb=200):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Calculate chunk size in bytes
+    chunk_size_bytes = chunk_size_mb * 1024 * 1024
+
+    with open(input_file_path, 'rb') as pgn_file:
+        chunk_number = 1
+        current_chunk_size = 0
+        current_chunk_path = os.path.join(output_folder, f"chunk_{chunk_number}.pgn")
+
+        with open(current_chunk_path, 'wb') as current_chunk:
+            while True:
+                data = pgn_file.read(1024)
+                if not data:
+                    break
+
+                current_chunk.write(data)
+                current_chunk_size += len(data)
+
+                # Check for the end of a game and create a new chunk
+                if current_chunk_size >= chunk_size_bytes and b'\n\n' in data:
+                    print(f"Chunk {chunk_number} saved to {current_chunk_path}")
+                    current_chunk_size = 0
+                    chunk_number += 1
+                    current_chunk_path = os.path.join(output_folder, f"chunk_{chunk_number}.pgn")
+                    current_chunk = open(current_chunk_path, 'wb')
+
+    print(f"Last chunk {chunk_number} saved to {current_chunk_path}")
+
+# Specify the path to your input PGN file
+input_pgn_file = "C:\\Users\\manas\\Coding Playground\\PythonP\\PGN_splitter\\lichess_db_standard_rated_2016-02_copy.pgn"
+
+# Specify the folder where you want to save the chunks
+output_folder = "C:\\Users\\manas\\Coding Playground\\PythonP\\PGN_splitter\\PGN_Chunks"
+
+# Specify the desired chunk size in MB
+chunk_size_mb = 200
+
+# Call the function to split the PGN file into chunks
+split_pgn_into_chunks(input_pgn_file, output_folder, chunk_size_mb)
